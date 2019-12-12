b48250c57d22a4c7effcefd3c1dd51c94915e132
Steve Litras <swl@litras.org>
2019-12-12 08:02:33 -0800

    still working on lambda failure issue

--- sam/functions/all_pages_handler.py
+++ sam/functions/all_pages_handler.py
@@ -27,7 +27,7 @@ def getAllWikiPages(config, params):
 
   murl = config['wiki']['url'] + '/rest/api/content?type=page&start=' + str(params['start']) + "&limit=" + str(params['limit'])
   logging.info("Retrieving Payload")
-  o = get_url(murl, auth=HTTPBasicAuth(config['wiki']['username'], config['wiki']['passwd']))
+  o = get_url(murl, auth=HTTPBasicAuth(config['wiki']['username'], config['wiki']['passwd']), timeout=300)
 
   logging.debug("Raw: " + o.text)
   logging.info("Retrieved Payload: " + str(o.status_code))
