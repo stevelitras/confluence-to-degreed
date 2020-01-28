a9b19ddf824ff3bb307d3c9224aa0b40d9afa01c
EC2 Default User <swl@litras.org>
2020-01-28 17:05:02 +0000

    building out the email functionality

--- sam/functions/email_notify.py
+++ sam/functions/email_notify.py
@@ -8,6 +8,9 @@ import csv
 import pysftp
 from collections import OrderedDict
 from requests.auth import HTTPBasicAuth
+from jinja2 import Template
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 # Setting up some pseudo constants
 LOG_LEVEL = logging.INFO if ("DEBUG" not in os.environ) else logging.DEBUG
@@ -22,9 +25,48 @@ formatter = logging.Formatter('%(asctime)s - %(message)s')
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
+# send_email sends the deprovisioning email, based on
+# specified parameters.
+def send_email(config, params):
+
+  try:
+    # Python 3
+    import urllib.request as urllib
+  except ImportError:
+    # Python 2
+    import urllib2 as urllib
+
+  if "sendgrid" not in config or "api_key" not in config['sendgrid']:
+    raise("No Sendgrid API Key in Parameters")
+  logging.debug("Using APIKEY: %s" % config['sendgrid']['api_key'])
+  sg = SendGridAPIClient(config['sendgrid']['api_key'])
+
+
+  to_mail = [ (params['to'], params['to']) ]
+  if ("cc" in params) and (params['cc'] != params['to']):
+    to_mail.append((params['cc'], params['cc']))
+  mail = Mail(from_email=params['from'],
+              subject=params['subject'],
+              to_emails=to_mail,
+              html_content=params['template'])
+
+  try:
+    response = sg.send(mail)
+    print(response.status_code)
+    print(response.body)
+  except Exception as e:
+    print(e)
+
+
+
 def lambda_handler(event, context):
   config = getParamInfo()
   template_values(config,config,tokens)
+  
+  subject_template = "Content Deleted from Degreed Pathway: {{ record.pathway_title }}"
+  body_template = "<P>Hi - </P><P>You are receiving this email because you are the creator of the Degreed Pathway: {{ record.pathway_title }}, and one of the lessons in that pathway is a wiki page that has been removed: {{ record.wiki_url }}. Please make any necessary changes to the pathway at your earliest convenience.</P><P>Thanks,<br>The Learning Admins</P>"
+ 
+  params = { }
   
   logging.info("Event: %s" % json.dumps(event))
   if not isinstance(event, list):
@@ -32,3 +74,22 @@ def lambda_handler(event, context):
   else:
     for record in event:
       logging.info("Would send email to %s, about pathway %s and url %s" % (record['send_to'],record['pathway_title'],record['wiki_url']))
+      params['to'] = "steve.litras@autodesk.com"
+      
+      try:
+        tpl = Template(subject_template)
+      except Exception as e:
+        logging.error("Error: %s" % e)
+
+      params['subject'] = tpl.render(record=record)
+      
+      try:
+        tpl = Template(body_template)
+      except Exception as e:
+        logging.error("Error: %s" % e)
+
+      params['body'] = tpl.render(record=record)
+      if "sendgrid" in config and "from" in config['sendgrid']:
+        params['from'] = config['sendgrid']['from']
+        
+      send_email(config, params)
